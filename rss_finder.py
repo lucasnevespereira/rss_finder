10111d782bcae57ace1b3ae79d1d57aad9adfe82
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-01-23 16:13:35 +0100

    fetched  possible rss urls

--- rss_finder.py
+++ rss_finder.py
@@ -17,7 +17,25 @@ def getWebsites():
         websites_list.append(link)
 
 
-print("Websites List: ", websites_list)
+def find_rss(site):
+    raw = requests.get(site).text
+    result = []
+    possible_feeds = []
+    html = bs(raw, features="html5lib")
+    feed_urls = html.findAll("link", rel="alternate")
+    if len(feed_urls) > 1:
+        for f in feed_urls:
+            t = f.get("type", None)
+            if t:
+                if "rss" in t or "xml" in t:
+                    href = f.get("href", None)
+                    if href:
+                        possible_feeds.append(href)
+                        print(href)
+
+
 print("Executing scraping...")
 getWebsites()
-print("Websites List: ", websites_list)
+
+for website in websites_list:
+    find_rss(website)
