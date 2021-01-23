2ef26538f69f25fa84b1bcd3e1d76dbdea3a1494
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-01-23 23:19:42 +0100

    improved rss finder

--- rss_finder.py
+++ rss_finder.py
@@ -3,24 +3,35 @@ from bs4 import BeautifulSoup as bs
 import feedparser
 import urllib.parse
 import validators
+import random
+
+
+A = ("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+     )
+
+Agent = A[random.randrange(len(A))]
+
+headers = {'user-agent': Agent}
 
 
 def init_finder(term):
-    search = "https://news.google.com/rss/search?q="+term+""
-    keyword = term
+
+    query = term
+    query = query.replace(' ', '+')
+    search = f"https://google.com/search?q={query}"
     websites_list = []
     result = []
 
     def getWebsites():
 
-        r = requests.get(search)
-        soup = bs(r.content, features="xml")
+        r = requests.get(search, headers=headers)
+        soup = bs(r.text, features="lxml")
 
-        articles = soup.find_all("item")
-
-        for article in articles:
-            link = article.find("link").text
-            websites_list.append(link)
+        for info in soup.find_all('div', class_='yuRUbf'):
+            link = info.find('a')
+            websites_list.append(link.get('href'))
 
     def find_rss(site):
         raw = requests.get(site).text
