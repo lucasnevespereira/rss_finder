c5a1bcbc4453a25750dbf509880af5ec834239c9
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-01-23 18:35:40 +0100

    finished flask app with working form

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template("index.html",)
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    term = request.form["search"]
+    return render_template("result.html", results=term)
 
 
 if __name__ == "__main__":
